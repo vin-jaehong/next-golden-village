a343835bca045d67dcf2c3ade029eddacec62b33
JaeHong <dnflwoghddl@gmail.com>
2024-03-01 18:17:37 +0900

    테스트 커밋

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,5 +1,5 @@
 const MovieDetail = ({ params: { id } }: { params: { id: string } }) => {
-  // props.params.{dynamic route value}
+  // props.params.{dynamic route value} 테스트
   return <h1>Movie {id}</h1>;
 };
 
