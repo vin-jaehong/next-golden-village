652dad585a8d5583c1ef2072d7e131bb2808e753
JaeHong <dnflwoghddl@gmail.com>
2024-03-06 00:53:27 +0900

    기존 fetch 방식으로 api request 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,26 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+
+import { useEffect, useState } from "react";
 
 const Page = () => {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+
+  // 첫 랜더링때만 실행
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
 };
 
 export default Page;
