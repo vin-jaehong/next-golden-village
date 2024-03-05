97f3dd5d89a6bbcbed3c9d2f88bf1bc86e12f779
JaeHong <dnflwoghddl@gmail.com>
2024-03-06 01:56:40 +0900

    서버 사이드에서 data fetch 하는 방법 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,26 +1,18 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-const Page = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-
-  // 첫 랜더링때만 실행
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
+export const metadata = {
+  title: "home",
 };
 
-export default Page;
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+const getMovies = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+};
+
+const HomePage = async () => {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
+};
+
+export default HomePage;
