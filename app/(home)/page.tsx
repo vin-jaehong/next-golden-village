f364bd1aaf1cf5e496e224574cefbd29fd445091
JaeHong <dnflwoghddl@gmail.com>
2024-03-06 02:17:26 +0900

    loading component 실습

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,8 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 const getMovies = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
   const response = await fetch(URL);
   const json = await response.json();
   return json;
