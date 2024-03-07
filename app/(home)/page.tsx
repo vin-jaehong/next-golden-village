a244d9de59b7977be0793a2cd1c65075d8c76c62
JaeHong <dnflwoghddl@gmail.com>
2024-03-07 23:41:01 +0900

    주석 추가

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,9 +2,10 @@ export const metadata = {
   title: "home",
 };
 
-// 노마드 코더쪽에서 제공해주는 API Path
+// 노마드 코더쪽에서 제공해주는 api path
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
+// api request function
 const getMovies = async () => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
@@ -13,6 +14,7 @@ const getMovies = async () => {
   return json;
 };
 
+// 컴포넌트를 async 함수로 만들고, API 요청을 할 경우 서버사이드에서 요청이 이루어짐.
 const HomePage = async () => {
   const movies = await getMovies();
   return <div>{JSON.stringify(movies)}</div>;
