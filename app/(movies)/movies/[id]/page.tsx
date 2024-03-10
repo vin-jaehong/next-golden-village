6ff989ae7b0b786320ef4a4a3c782cb468e1e046
JaeHong <dnflwoghddl@gmail.com>
2024-03-11 00:32:31 +0900

    parallel request 관련 주석 추가

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -5,6 +5,7 @@ export const metadata: Metadata = {
   title: "movie",
 };
 
+// 영화 정보 api request method
 const getMovie = async (id: string) => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
@@ -12,13 +13,16 @@ const getMovie = async (id: string) => {
   return response.json();
 };
 
+// 영화 예고편 api request method
 const getVideos = async (id: string) => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 };
 
+// 영화 정보 페이지 컴포넌트
 const MovieDetail = async ({ params: { id } }: { params: { id: string } }) => {
+  // parallel request (병렬 요청)으로 병렬로 fetch 처리
   const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
 
   return <h1>{movie.title}</h1>;
