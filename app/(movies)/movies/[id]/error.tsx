7e52cf4d7d51edece712e95f4ea930c40172aa4a
JaeHong <dnflwoghddl@gmail.com>
2024-03-13 01:15:12 +0900

    페이지 에러 핸들링 추가

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const ErrorOMG = () => {
+  return <div>lol something broke....</div>;
+};
+
+export default ErrorOMG;
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,9 +2,10 @@ import { API_URL } from "../app/(home)/page";
 
 // 영화 예고편 api request method
 const getVideos = async (id: string) => {
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  throw new Error("something broke...");
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // return response.json();
 };
 
 const MovieVideos = async ({ id }: { id: string }) => {
