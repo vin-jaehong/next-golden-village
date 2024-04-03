4b779876df366d149d90d152ed2e4ee55b079331
JaeHong <dnflwoghddl@gmail.com>
2024-04-03 15:30:13 +0900

    not found page component comment delete

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,9 @@
 import { Metadata } from "next";
 
-// ** 메타데이터 설정
 export const metadata: Metadata = {
   title: "Not found Page!",
 };
 
-// ** 존재하지 않는 페이지 접근 시 랜더링 되는 컴포넌트
 const NotFound = () => {
   return (
     <div>
