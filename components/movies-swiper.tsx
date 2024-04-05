47990cbedd6ab1e79da5a270d8c7712a2f67eacd
JaeHong <dnflwoghddl@gmail.com>
2024-04-05 18:31:09 +0900

    movie swiper comment added

--- components/movies-swiper.tsx
+++ components/movies-swiper.tsx
@@ -13,7 +13,7 @@ import { useRef } from "react";
 const MoviesSwiper = ({ movies }) => {
   const swiperRef = useRef(null);
 
-  // !! custom navigation button 이 먹히지 않아 trigger 메소드로 작업
+  // ** custom navigation button 이 먹히지 않아 trigger 메소드로 작업
   const swiperSlidePrev = () => {
     swiperRef.current.swiper.slidePrev();
   };
