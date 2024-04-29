21807248e709623d32354f377641e7c220852474
JaeHong <dnflwoghddl@gmail.com>
2024-04-29 15:44:53 +0900

    add comment

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,11 +1,17 @@
 import { API_URL } from "../constants";
 import styles from "../styles/movie-info.module.css";
 
+/**
+ * request movie information method
+ * @param id
+ * @returns
+ */
 export const getMovie = async (id: string) => {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 };
 
+// movie information component
 const MovieInfo = async ({ id }: { id: string }) => {
   const movie = await getMovie(id);
 
