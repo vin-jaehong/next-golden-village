910e55bf5a9e9a5d2688d69f67c1901dd0bc6970
JaeHong <dnflwoghddl@gmail.com>
2024-05-09 16:29:03 +0900

    delete comment

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -7,7 +7,6 @@ const getVideos = async (id: string) => {
   return response.json();
 };
 
-// movie videos component
 const MovieVideos = async ({ id }: { id: string }) => {
   const videos = await getVideos(id);
   return (
