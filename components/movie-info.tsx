b39eb01eb3b1c154a8ea081b304c7b7b3843f8f1
JaeHong <dnflwoghddl@gmail.com>
2024-05-17 16:12:17 +0900

    add comment

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -11,6 +11,7 @@ export const getMovie = async (id: string) => {
   return response.json();
 };
 
+// movie information component
 const MovieInfo = async ({ id }: { id: string }) => {
   const movie = await getMovie(id);
 
